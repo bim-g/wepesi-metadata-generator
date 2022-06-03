85f17679e4127f30f888a6e7854c23e7c0f123e9
bim-g <ibmussafb@gmail.com>
2022-06-03 23:54:44 +0200

    [Test] Simple test demo

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 require "vendor/autoload.php";
 use Wepesi\MetaData;
 
-$meta= MetaData::generate()->title('Welcom to wepesi')->structure();
+$meta= MetaData::generate()->title('Welcom To our Article')
+    ->descriptions('About Description of the article')
+    ->lang('sw')
+    ->type('article')
+    ->link('http://www.domaine.com/about')
+    ->cover('http://www.domaine.com/article-cover/cover.jpg')
+    ->index()
+    ->follow()
+    ->structure();
 var_dump($meta);
